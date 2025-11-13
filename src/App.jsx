497678feac41f72c799232147678401733f1bb9c
Jeong In Jun <jijun897@naver.com>
2025-11-13 16:55:23 +0900

    feat: 2차 메인 부분 분리한 코드 준비

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Global } from "@emotion/react";
 import styled from "@emotion/styled";
 import { Top5Movies } from "./api/tmdbApi";
 import { globalStyles } from "./styles/globalStyle";
-import { MovieCard } from "./components/MovieCard";
 import { TheHeader } from "./components/TheHeader";
+import { MainPage } from "./components/MainPage";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
@@ -27,17 +27,7 @@ export default function App() {
     <Container>
       <Global styles={globalStyles} />
       <TheHeader setMovies={setMovies} setTitle={setTitle} setError={setError} />
-      <Main>
-        <Title>{title}</Title>
-        <ErrorMessage>{error}</ErrorMessage>
-        <Grid>
-          {movies.map((m) => (
-            <MovieCard key={m.id} movie={m} />
-          ))}
-        </Grid>
-      </Main>
-
-      <Footer>2025 Movie Search Project</Footer>
+      <MainPage movies={movies} title={title} error={error} />
     </Container>
   );
 }
@@ -46,54 +36,4 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-`;
-
-const Main = styled.main`
-  padding: 10px;
-  flex: 1 0 auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #f4f4f4;
-`;
-
-const Grid = styled.section`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  gap: 50px;
-  width: 100%;
-  max-width: 2000px;
-  margin-top: 16px;
-
-  @media (max-width: 1600px) {
-    grid-template-columns: repeat(4, minmax(180px, 1fr));
-  }
-  @media (max-width: 1200px) {
-    grid-template-columns: repeat(3, minmax(180px, 1fr));
-  }
-  @media (max-width: 800px) {
-    grid-template-columns: repeat(2, minmax(180px, 1fr));
-  }
-`;
-
-const Footer = styled.footer`
-  height: 4vh;
-  background-color: #252525;
-  color: white;
-  text-align: center;
-  width: 100%;
-  padding: 8px 0;
-`;
-
-const Title = styled.h1`
-  font-size: 24px;
-  font-weight: 700;
-  margin-top: 24px;
-  margin-bottom: 0;
-`;
-
-const ErrorMessage = styled.p`
-  font-size: 20px;
-  color: #c0392b;
-  font-weight: 700;
 `;
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,57 @@
+import styled from "@emotion/styled";
+import { MovieCard } from "./MovieCard";
+
+export const MainPage = ({ movies, title, error }) => {
+  return (
+    <Main>
+      <Title>{title}</Title>
+      <ErrorMessage>{error}</ErrorMessage>
+      <Grid>
+        {movies.map((m) => (
+          <MovieCard key={m.id} movie={m} />
+        ))}
+      </Grid>
+    </Main>
+  );
+};
+
+const Main = styled.main`
+  padding: 10px;
+  flex: 1 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f4f4f4;
+`;
+
+const Grid = styled.section`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 50px;
+  width: 100%;
+  max-width: 2000px;
+  margin-top: 16px;
+
+  @media (max-width: 1600px) {
+    grid-template-columns: repeat(4, minmax(180px, 1fr));
+  }
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(3, minmax(180px, 1fr));
+  }
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(2, minmax(180px, 1fr));
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 700;
+  margin-top: 24px;
+  margin-bottom: 0;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 20px;
+  color: #c0392b;
+  font-weight: 700;
+`;
